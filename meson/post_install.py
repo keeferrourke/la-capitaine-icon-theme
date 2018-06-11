aaaf104cb633e38cee89df81e208272c9f439ef8
Igor Dyatlov <dyatlov.igor@gmail.com>
2018-06-12 01:22:15 +0300

    Installer: added meson installation script

---
+++ meson/post_install.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+from os import environ, path
+from subprocess import call
+
+if not environ.get('DESTDIR', ''):
+    PREFIX = environ.get('MESON_INSTALL_PREFIX', '/usr')
+    print('Updating icon cache...')
+    call(['gtk-update-icon-cache', '-qtf', path.join(PREFIX, 'share/icons/La-Capitaine')])
